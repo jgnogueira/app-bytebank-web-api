95dfa740be54ba74e208e5cfc9809cecf9ba6ba3
jgnogueira <josegmnogueira@hotmail.com>
2021-02-23 20:17:47 -0300

    Alterando lista para exibir transferências

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,38 +1,68 @@
+import 'package:bytebank_armazenamento_interno/http/webclient.dart';
 import 'package:bytebank_armazenamento_interno/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = List();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+      body: FutureBuilder<List<Transaction>>(
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text('Loading...'),
+                    ),
+                  ],
                 ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Transaction> transactions = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Transaction transaction = transactions[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.monetization_on),
+                      title: Text(
+                        transaction.value.toString(),
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        transaction.contact.accountNumber.toString(),
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+              );
+              break;
+          }
+
+          return Text('Unknown Error');
         },
-        itemCount: transactions.length,
       ),
     );
   }
