aea73b113f8263ea4885548fb2894549b14977ba
jgnogueira <josegmnogueira@hotmail.com>
2021-02-22 22:48:46 -0300

    Criando classe para interceptar requisições http

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,7 +1,23 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.118:8080/transactions');
-  print(response.body);
-  print(response.statusCode);
+  final Client client =
+      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response =
+      await client.get('http://192.168.0.118:8080/transactions');
 }
