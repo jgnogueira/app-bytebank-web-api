bf6e281f2c6dafad68d1727370f3e81ee928f56d
jgnogueira <josegmnogueira@hotmail.com>
2021-02-24 00:31:21 -0300

    Refatorando código para serializar Json

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -9,6 +9,14 @@ class Contact {
     this.accountNumber,
   );
 
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'accountNumber': accountNumber};
+
   @override
   String toString() {
     return 'Contact: $id - $name - $accountNumber';
